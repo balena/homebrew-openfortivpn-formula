fe65bc5ee33067447394b05cb39315e8ce2fe5f5
Guilherme Balena Versiani <guilherme@telnyx.com>
2020-07-15 14:42:24 -0300

    Upgraded to version 1.4.1

--- HomebrewFormula/openfortivpn.rb
+++ HomebrewFormula/openfortivpn.rb
@@ -1,8 +1,8 @@
 class Openfortivpn < Formula
   desc "Open Fortinet client for PPP+SSL VPN tunnel services"
   homepage "https://github.com/adrienverge/openfortivpn"
-  url "https://github.com/adrienverge/openfortivpn/archive/v1.14.0.tar.gz"
-  sha256 "3144056a93991ff77d52467a18ff72ab6c3349ebcea8642ceb3136c1091e3b98"
+  url "https://github.com/adrienverge/openfortivpn/archive/v1.14.1.tar.gz"
+  sha256 "bc62fc6ecaaa6c6f8f2510e14a067a0cb9762158d9460c04555990bba44b50ca"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
